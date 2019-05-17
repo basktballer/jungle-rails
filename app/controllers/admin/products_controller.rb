f1dbbd695beae03b7cc53ac4ef3e53fbb04791ce
Eden Yeung <basktballer@gmail.com>
2019-05-17 00:35:31 +0000

    Http admin login fixed

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['ADMIN_HTTP_USERNAME'], password: ENV['ADMIN_HTTP_PASSWORD']
+
   def index
     @products = Product.order(id: :desc).all
   end
