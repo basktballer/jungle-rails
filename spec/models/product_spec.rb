b400de7d92d7aee4a4723d266b24816acc4005c8
Eden Yeung <basktballer@gmail.com>
2019-05-20 17:43:43 +0000

    Tests created for product validations

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  subject { described_class.new( name: "test name", price: 200, quantity: 200, category: Category.first.name)}
+  describe 'Validations' do
+
+    before do 
+      @category = Category.new
+      @product = Product.new( :name => "test name", :price => 200, :quantity => 200, :category => @category)
+    end
+
+    it "is valid with valid atrributes" do
+      expect(@product).to be_valid
+    end
+    it "is not valid without a name" do
+      @product.name = nil
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+    it "is not valid without a price" do
+      @product.price_cents = nil
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+    it "is not valid without a quantity" do
+      @product.quantity = nil
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+    it "is not valid without a category" do
+      @product.category = nil
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
+  end
+end
